8dd53774f69a02ff6a806fe60b6dec639ffa8c1f
suoludare <oludaresunday07@gmail.com>
2026-02-27 14:59:46 +0000

    tutorials-go

--- examples/withdep/main.go
+++ examples/withdep/main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"github.com/fatih/color"
+	"github.com/peace/color"
 )
 
 func main() {
 	// print a colored message using a tiny third-party package
-	color.New(color.FgGreen).Println("Hello with color from fatih/color")
+	color.New(color.FgGreen).Println("Hello with color from peace/color")
 }
