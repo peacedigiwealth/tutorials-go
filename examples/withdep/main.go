4f0f36787273257914c1dbed41fb1cf92d8901c5
suoludare <oludaresunday07@gmail.com>
2026-02-27 14:52:21 +0000

    fix: examples/withdep main.go clean content

--- examples/withdep/main.go
+++ examples/withdep/main.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-    "github.com/fatih/color"
+	"github.com/fatih/color"
 )
 
 func main() {
-    // print a colored message using a tiny third-party package
-    color.New(color.FgGreen).Println("Hello with color from fatih/color")
+	// print a colored message using a tiny third-party package
+	color.New(color.FgGreen).Println("Hello with color from fatih/color")
 }
-package withdep
