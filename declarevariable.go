b8a624b66ac8b926b5c4aaef17d2d780084bcf2d
suoludare <oludaresunday07@gmail.com>
2026-03-01 16:54:52 +0000

    declarevariable.go

---
+++ declarevariable.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	//	Declare variables with zero values
+	var a string
+	var b int
+	var c bool
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+}
