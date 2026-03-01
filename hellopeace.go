66e2c0ef4d9175da7568678bef3954f81cda017d
suoludare <oludaresunday07@gmail.com>
2026-03-01 16:56:31 +0000

    hellopeace.go

--- hellopeace.go
+++ hellopeace.go
@@ -4,13 +4,4 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, Peace!")
-
-	//	Declare variables with explicit type and inferred type
-	var student1 string = "John" //type is string
-	var student2 = "Jane"        //type is inferred
-	x := 2                       //type is inferred
-
-	fmt.Println(student1)
-	fmt.Println(student2)
-	fmt.Println(x)
 }
